43fe50c91642dd060f66a3074d19b62ff9a42bcd
Danilo Freitas <danilo.freitas@familiapires.com.br>
2024-01-06 20:07:40 -0300

    feat: enable cors to api prefix

--- config/cors.ts
+++ config/cors.ts
@@ -1,7 +1,7 @@
 import type { CorsConfig } from '@ioc:Adonis/Core/Cors'
 
 const corsConfig: CorsConfig = {
-  enabled: false,
+  enabled: (request) => request.url().startsWith('/api/v1'),
   origin: true,
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
