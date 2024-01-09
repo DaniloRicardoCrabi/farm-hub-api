ceff63c106dc1755121cd11ad528329dc8b3f92a
Danilo Freitas <danilo.freitas@familiapires.com.br>
2024-01-08 22:47:30 -0300

    feat: add health check

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.group(() => {
+  Route.get('health', async ({ response }) => {
+    const report = await HealthCheck.getReport()
+    return report.healthy ? response.ok(report) : response.badRequest(report)
+  })
   Route.resource('producers', 'ProducersController').apiOnly()
   Route.resource('farms', 'FarmsController').apiOnly().except(['store'])
   Route.post('farms/:producerId', 'FarmsController.store')
