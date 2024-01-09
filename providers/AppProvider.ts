ff86cd21dd7063472d48dbfcc394c030096a7c4b
Danilo Freitas <danilo.freitas@familiapires.com.br>
2024-01-08 22:01:35 -0300

    feat: add logs at app init

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,6 +1,8 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class AppProvider {
+  private logger = Logger.child({ name: 'AppProvider' })
   constructor(protected app: ApplicationContract) {}
 
   public register() {
@@ -16,7 +18,26 @@ export default class AppProvider {
   }
 
   public async ready() {
-    // App is ready
+    this.logger.info('App is ready')
+    this.logger.info(
+      'HOST: ' + JSON.stringify(this.app.container.use('Adonis/Core/Env').get('HOST'))
+    )
+    this.logger.info(
+      'PORT: ' + JSON.stringify(this.app.container.use('Adonis/Core/Env').get('PORT'))
+    )
+    this.logger.info(
+      'HOST: ' + JSON.stringify(this.app.container.use('Adonis/Core/Env').get('NODE_ENV'))
+    )
+    this.logger.info(
+      'DATA BASE HOST: ' + JSON.stringify(this.app.container.use('Adonis/Core/Env').get('PG_HOST'))
+    )
+    this.logger.info(
+      'DATA BASE PORT: ' + JSON.stringify(this.app.container.use('Adonis/Core/Env').get('PG_PORT'))
+    )
+    this.logger.info(
+      'DATA BASE NAME: ' +
+        JSON.stringify(this.app.container.use('Adonis/Core/Env').get('PG_DB_NAME'))
+    )
   }
 
   public async shutdown() {
